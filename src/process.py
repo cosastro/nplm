d0ac94a90c4997cf8057b79ffb335419c4020eae
Singletone <yuriy.filonov@gmail.com>
2015-09-01 20:56:55 +1000

    Filter improvements.

--- src/process.py
+++ src/process.py
@@ -9,7 +9,12 @@ class WikipediaPage():
     def __init__(self, title, text):
         self.title = title
         self.text = text
-        self.isArticle = ':' not in self.title
+
+        mayReferTo = '{0} may refer to'.format(title).lower()
+        self.isArticle = len(text) > 2500 \
+                         and ':' not in self.title \
+                         and not text.startswith(mayReferTo) \
+                         and not text.startswith('#redirect')
 
     @staticmethod
     def create(title, text):
@@ -22,15 +27,26 @@ class WikipediaPage():
             if heading.lower() not in WikipediaPage.restrictedHeaders:
                 text += paragraph.lower()
 
+        text = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', 'URL', text)
         text = re.sub('\([^\)]+\)', '', text)
         text = re.sub('(:[^\.]\.)', '', text)
-        text = re.sub('[,":]', ' ', text)
+        text = re.sub('[,":_\*]', ' ', text)
+        text = re.sub('!', '.', text)
+        text = re.sub('\?', '.', text)
         text = re.sub('\s(\.{4,})\s', ' ', text)
         text = re.sub('\s(\.{3})\s', '.', text)
         text = re.sub('\s(\.{2})\s', ' ', text)
         text = re.sub('[^a-z]+([0-9\-]+)[^a-z]+', ' NUMBER ', text)
-        text = re.sub('\s([^a-zA-Z0-9_\.\-\s]+)\s', ' SYMBOL ', text)
+        text = re.sub('\s([^a-zA-Z0-9\.\-\s]+)\s', ' SYMBOL ', text)
         text = re.sub('\s([bcdefghjklmnopqrstuvwxyz])\s', ' SYMBOL ', text)
+
+        sentences = re.split('[(\n{2,})\.;]', text)
+        sentences = [re.sub('[\s]+', ' ', sentence).strip() for sentence in sentences]
+        sentences = [sentence for sentence in sentences
+                     if len(sentence.split(' ')) > 5 \
+                     and sentence.count('NUMBER') < 3]
+
+        text = '. '.join(sentences)
 
         return WikipediaPage(title, text)
     
@@ -56,11 +72,10 @@ class WikipediaDumpFile():
 
 
 testFilePath = '../data/Wikipedia/20140615-wiki-en_000022.txt.gz'
-#testFilePath = 'test.txt.gz'
 pages = WikipediaDumpFile.load(testFilePath)
 
 for page in pages:
     print '{0}: {1}'.format(page.title, len(page.text))
     
-    pageFilePath = 'dumps/{0}.txt.gz'.format(uuid.uuid1())
+    pageFilePath = '../data/Wikipedia-pages/{0}.txt.gz'.format(uuid.uuid1())
     page.dump(pageFilePath)
