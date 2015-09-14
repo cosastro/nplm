02776ea91f5d5dc715fe400335969dbb000bc47c
Yuriy Filonov <yuriy.filonov@gmail.com>
2015-09-14 21:35:21 +1000

    Rubenstein-Goodenough coefficient calculation.

--- src/runevaluation.py
+++ src/runevaluation.py
@@ -1,4 +1,6 @@
 from processgooglenews import *
+import scipy.stats
+import numpy
 
 
 def loadEmbeddings(embeddingsFilePath, stringBased):
@@ -8,8 +10,34 @@ def loadEmbeddings(embeddingsFilePath, stringBased):
     return None
 
 
-def evaluateRubensteinGoodenough(embeddings, filePath):
-    return 0
+def evaluateRubensteinGoodenough(embeddings, filePath, base=10):
+    with open(filePath) as file:
+        lines = file.readlines()
+
+    wordPairs = []
+    targetScores = []
+    for line in lines:
+        word1, word2, score = tuple(line.strip().split('\t'))
+        score = float(score)
+
+        wordPairs.append((word1, word2))
+        targetScores.append(score)
+
+    scores = []
+    for word1, word2 in wordPairs:
+        word1Embedding = embeddings[word1]
+        word2Embedding = embeddings[word2]
+
+        score = cosineSimilarity(word1Embedding, word2Embedding)
+        scores.append(score)
+
+    pearson, pearsonDeviation = scipy.stats.pearsonr(scores, targetScores)
+    spearman, spearmanDeviation = scipy.stats.spearmanr(scores, targetScores)
+
+    rubensteinGoodenoughMetric = numpy.mean([pearson, spearman]) * base
+    rubensteinGoodenoughMetricMax = 0.915 * base
+
+    return rubensteinGoodenoughMetric, rubensteinGoodenoughMetricMax
 
 
 def evaluateWordSimilarity353(embeddings, filePath):
@@ -33,8 +61,8 @@ if __name__ == '__main__':
 
     embeddings = loadEmbeddings(embeddingsFilePath, True)
 
-    rubensteinGoodenoughFilePath = '../data/RG/EN-RG-65.txt'
-    rubensteinGoodenoughMetric = evaluateRubensteinGoodenough(embeddings, rubensteinGoodenoughFilePath)
+    rgFilePath = '../data/RG/EN-RG-65.txt'
+    rgMetric, rgStateOfTheArt = evaluateRubensteinGoodenough(embeddings, rgFilePath)
 
     wordSimilarity353FilePath = '../data/WordSimilarity-353/combined.csv'
     wordSimilarity353Metric = evaluateWordSimilarity353(embeddings, wordSimilarity353FilePath)
@@ -48,7 +76,7 @@ if __name__ == '__main__':
     satQuestionsFilePath = '../data/SAT-Questions/SAT-package-V3.txt'
     satQuestionsMetric = evaluateSATQuestions(embeddings, satQuestionsFilePath)
 
-    log.info('Rubenstein-Goodenough: {0}/10'.format(rubensteinGoodenoughMetric))
+    log.info('Rubenstein-Goodenough: {0:.2f}/10. State of the art: {1:.2f}/10'.format(rgMetric, rgStateOfTheArt))
     log.info('WordSimilarity-353: {0}/10'.format(wordSimilarity353Metric))
     log.info('SimLex-999: {0}/10'.format(simLex999Metric))
     log.info('Syntactic word relations: {0}/10'.format(syntacticWordRelationsMetric))
