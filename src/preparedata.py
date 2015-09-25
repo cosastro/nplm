9bbe3fa03e823e7dbf9226ae31edc95b5bf38cd3
Yuriy Filonov <yuriy.filonov@gmail.com>
2015-09-25 21:46:42 +1000

    Moved text cleaning to a new class.

--- src/preparedata.py
+++ src/preparedata.py
@@ -3,6 +3,8 @@ import shutil
 import log
 import glob
 import time
+import gzip
+import re
 from pathos import multiprocessing
 from datetime import timedelta
 
@@ -13,26 +15,110 @@ class WikipediaPreparator():
         self.flags = None
 
 
-    def saveWikipediaPage(outputDirectoryPath, dumpIndex, dumpName, pageName, pageText, compress):
-        pass
+    def filterPage(self, page):
+        pageName, pageText = page
+
+        if ':' in pageName:
+            return False
+
+        mayReferTo = '{0} may refer to'.format(pageName).lower()
+        if pageText.startswith(mayReferTo):
+            return False
+
+        if pageText.startswith('#redirect'):
+            return False
+
+        if len(pageText) < 2500:
+            return False
+
+        return True
 
 
-    def unpackWikipediaDump(self, dumpIndex, dumpPath, outputDirectoryPath, filterText, compress):
+    def cleanPage(self, page):
+        pageName, pageText = page
+        restrictedHeaders = ['see also', 'footnotes', 'references', 'further reading', 'external links', 'books']
+
+        headings = [pageName] + re.findall('^=+\s*([^=]+)\s*=+$', pageText, flags=re.M)
+        paragraphs = re.split('^=+\s*[^=]+\s*=+$', pageText, flags=re.M)
+
+        pageText = ''
+
+        for heading, paragraph in zip(headings, paragraphs):
+            if heading.lower() not in restrictedHeaders:
+                pageText += paragraph.lower()
+
+        pageText = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', 'URL', pageText)
+        pageText = re.sub('\([^\)]+\)', '', pageText)
+        pageText = re.sub('(:[^\.]\.)', '', pageText)
+        pageText = re.sub('[,":_\*]', ' ', pageText)
+        pageText = re.sub('!', '.', pageText)
+        pageText = re.sub('\?', '.', pageText)
+        pageText = re.sub('\s(\.{4,})\s', ' ', pageText)
+        pageText = re.sub('\s(\.{3})\s', '.', pageText)
+        pageText = re.sub('\s(\.{2})\s', ' ', pageText)
+        pageText = re.sub('<[^>]+>', '', pageText)
+        pageText = re.sub('[^a-z]+([0-9\-]+)[^a-z]+', ' NUMBER ', pageText)
+        pageText = re.sub('\s([^a-zA-Z0-9\.\-\s]+)\s', ' SYMBOL ', pageText)
+        pageText = re.sub('\s([bcdefghjklmnopqrstuvwxyz])\s', ' SYMBOL ', pageText)
+
+        sentences = re.split('[(\n{2,})\.;]', pageText)
+        sentences = [re.sub('[\s]+', ' ', sentence).strip() for sentence in sentences]
+        sentences = [sentence for sentence in sentences
+                     if len(sentence.split(' ')) > 5 and sentence.count('NUMBER') < 3]
+
+        pageText = '. '.join(sentences)
+
+        return pageName, pageText
+
+
+    def savePage(self, dumpDirectoryPath, pageName, pageText, compress):
+        filePath = os.path.join(dumpDirectoryPath, pageName + '.txt')
+
+        if compress:
+            filePath = filePath + '.gz'
+            with gzip.open(filePath, 'wb+') as file:
+                file.write(pageText)
+        else:
+            with open(filePath, 'wb+') as file:
+                file.write(pageText)
+
+
+    def unpackDump(self, dumpIndex, dumpPath, outputDirectoryPath, cleanText, compress):
         dumpName = os.path.basename(dumpPath).split('.')[0]
         pages = []
         currentTime = time.time()
         elapsed = currentTime - self.startTime
         secondsPerFile = elapsed
 
-        log.progress('Unpacking Wikipedia dumps: {0:.3f}%. Last dump: {1} ({2} pages). Elapsed: {3:2}. ({4:.3f} sec/file)',
-                     dumpIndex + 1,
-                     self.dumpsCount,
-                     dumpName,
-                     len(pages),
-                     timedelta(seconds=elapsed),
-                     secondsPerFile)
+        try:
+            with gzip.open(dumpPath, 'rb') as dumpFile:
+                dumpText = dumpFile.read()
 
-        time.sleep(0.1)
+            dumpDirectoryPath = os.path.join(outputDirectoryPath, dumpName)
+            os.mkdir(dumpDirectoryPath)
+            os.chown(dumpDirectoryPath, 1000, 1000)
+
+            pageNames = [name.strip() for name in re.findall('^\[\[(?P<title>[^\]]+)\]\]\s?$', dumpText, flags=re.M)]
+            pageTexts = [pageText.strip() for pageText in re.split('^\[\[[^\]]+\]\]\s?$', dumpText, flags=re.M) if pageText]
+
+            pages = zip(pageNames, pageTexts)
+            pages = filter(self.filterPage, pages)
+
+            if cleanText:
+                pages = map(self.cleanPage, pages)
+
+            for pageName, pageText in pages:
+                self.savePage(dumpDirectoryPath, pageName, pageText, compress)
+
+            log.progress('Unpacking Wikipedia dumps: {0:.3f}%. Last dump: {1} ({2} pages). Elapsed: {3}. ({4:.3f} sec/file)',
+                         dumpIndex + 1,
+                         self.dumpsCount,
+                         dumpName,
+                         len(pages),
+                         timedelta(seconds=elapsed),
+                         secondsPerFile)
+        except:
+            pass
 
 
     def prepare(self, inputDirectoryPath, outputDirectoryPath, filterText=True, compress=True):
@@ -44,7 +130,7 @@ class WikipediaPreparator():
         os.chown(outputDirectoryPath, 1000, 1000)
         log.info('Output directory {0} has been created.', outputDirectoryPath)
 
-        pathName = inputDirectoryPath + '/**/*wiki*.txt.gz'
+        pathName = inputDirectoryPath + '/*wiki*.txt.gz'
         dumpPaths = glob.glob(pathName)
         self.dumpsCount = len(dumpPaths)
         log.info('Found {0} Wikipedia dumps.', self.dumpsCount)
@@ -52,7 +138,7 @@ class WikipediaPreparator():
         processes = multiprocessing.cpu_count()
         pool = multiprocessing.Pool(processes=processes)
         for dumpIndex, dumpPath in enumerate(dumpPaths):
-            pool.apply(self.unpackWikipediaDump, args=(dumpIndex, dumpPath, outputDirectoryPath, filterText, compress))
+            pool.apply_async(self.unpackDump, args=(dumpIndex, dumpPath, outputDirectoryPath, filterText, compress))
         pool.close()
         pool.join()
 
