e0a8eaf8533f74ef22a9dcdc0cad54595f9a72bb
Yuriy Filonov <yuriy.filonov@gmail.com>
2015-09-26 11:09:09 +1000

    Got rid of OOP stuff for the sake of simplicity.

--- src/preparedata.py
+++ src/preparedata.py
@@ -8,146 +8,144 @@ import re
 from pathos import multiprocessing
 from datetime import timedelta
 
-class WikipediaPreparator():
-    def __init__(self):
-        self.startTime = time.time()
-        self.dumpsCount = 0
-        self.flags = None
+
+def filterPage(page):
+    pageName, pageText = page
+
+    if ':' in pageName:
+        return False
+
+    mayReferTo = '{0} may refer to'.format(pageName).lower()
+    if pageText.startswith(mayReferTo):
+        return False
+
+    if pageText.startswith('#redirect'):
+        return False
+
+    if len(pageText) < 2500:
+        return False
+
+    return True
 
 
-    def filterPage(self, page):
-        pageName, pageText = page
+def cleanPage(page):
+    pageName, pageText = page
+    restrictedHeaders = ['see also', 'footnotes', 'references', 'further reading', 'external links', 'books']
 
-        if ':' in pageName:
-            return False
+    headings = [pageName] + re.findall('^=+\s*([^=]+)\s*=+$', pageText, flags=re.M)
+    paragraphs = re.split('^=+\s*[^=]+\s*=+$', pageText, flags=re.M)
 
-        mayReferTo = '{0} may refer to'.format(pageName).lower()
-        if pageText.startswith(mayReferTo):
-            return False
+    pageText = ''
 
-        if pageText.startswith('#redirect'):
-            return False
+    for heading, paragraph in zip(headings, paragraphs):
+        if heading.lower() not in restrictedHeaders:
+            pageText += paragraph.lower()
 
-        if len(pageText) < 2500:
-            return False
+    pageText = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', 'URL', pageText)
+    pageText = re.sub('\([^\)]+\)', '', pageText)
+    pageText = re.sub('(:[^\.]\.)', '', pageText)
+    pageText = re.sub('[,":_\*]', ' ', pageText)
+    pageText = re.sub('!', '.', pageText)
+    pageText = re.sub('\?', '.', pageText)
+    pageText = re.sub('\s(\.{4,})\s', ' ', pageText)
+    pageText = re.sub('\s(\.{3})\s', '.', pageText)
+    pageText = re.sub('\s(\.{2})\s', ' ', pageText)
+    pageText = re.sub('<[^>]+>', '', pageText)
+    pageText = re.sub('[^a-z]+([0-9\-]+)[^a-z]+', ' NUMBER ', pageText)
+    pageText = re.sub('\s([^a-zA-Z0-9\.\-\s]+)\s', ' SYMBOL ', pageText)
+    pageText = re.sub('\s([bcdefghjklmnopqrstuvwxyz])\s', ' SYMBOL ', pageText)
 
-        return True
+    sentences = re.split('[(\n{2,})\.;]', pageText)
+    sentences = [re.sub('[\s]+', ' ', sentence).strip() for sentence in sentences]
+    sentences = [sentence for sentence in sentences
+                 if len(sentence.split(' ')) > 5 and sentence.count('NUMBER') < 3]
 
+    pageText = '. '.join(sentences)
 
-    def cleanPage(self, page):
-        pageName, pageText = page
-        restrictedHeaders = ['see also', 'footnotes', 'references', 'further reading', 'external links', 'books']
-
-        headings = [pageName] + re.findall('^=+\s*([^=]+)\s*=+$', pageText, flags=re.M)
-        paragraphs = re.split('^=+\s*[^=]+\s*=+$', pageText, flags=re.M)
-
-        pageText = ''
-
-        for heading, paragraph in zip(headings, paragraphs):
-            if heading.lower() not in restrictedHeaders:
-                pageText += paragraph.lower()
-
-        pageText = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', 'URL', pageText)
-        pageText = re.sub('\([^\)]+\)', '', pageText)
-        pageText = re.sub('(:[^\.]\.)', '', pageText)
-        pageText = re.sub('[,":_\*]', ' ', pageText)
-        pageText = re.sub('!', '.', pageText)
-        pageText = re.sub('\?', '.', pageText)
-        pageText = re.sub('\s(\.{4,})\s', ' ', pageText)
-        pageText = re.sub('\s(\.{3})\s', '.', pageText)
-        pageText = re.sub('\s(\.{2})\s', ' ', pageText)
-        pageText = re.sub('<[^>]+>', '', pageText)
-        pageText = re.sub('[^a-z]+([0-9\-]+)[^a-z]+', ' NUMBER ', pageText)
-        pageText = re.sub('\s([^a-zA-Z0-9\.\-\s]+)\s', ' SYMBOL ', pageText)
-        pageText = re.sub('\s([bcdefghjklmnopqrstuvwxyz])\s', ' SYMBOL ', pageText)
-
-        sentences = re.split('[(\n{2,})\.;]', pageText)
-        sentences = [re.sub('[\s]+', ' ', sentence).strip() for sentence in sentences]
-        sentences = [sentence for sentence in sentences
-                     if len(sentence.split(' ')) > 5 and sentence.count('NUMBER') < 3]
-
-        pageText = '. '.join(sentences)
-
-        return pageName, pageText
+    return pageName, pageText
 
 
-    def savePage(self, dumpDirectoryPath, pageName, pageText, compress):
-        filePath = os.path.join(dumpDirectoryPath, pageName + '.txt')
+def savePage(dumpDirectoryPath, pageName, pageText, compress):
+    filePath = os.path.join(dumpDirectoryPath, pageName + '.txt')
 
-        if compress:
-            filePath = filePath + '.gz'
-            with gzip.open(filePath, 'wb+') as file:
-                file.write(pageText)
-        else:
-            with open(filePath, 'wb+') as file:
-                file.write(pageText)
+    if compress:
+        filePath = filePath + '.gz'
+        with gzip.open(filePath, 'wb+') as file:
+            file.write(pageText)
+    else:
+        with open(filePath, 'wb+') as file:
+            file.write(pageText)
 
 
-    def unpackDump(self, dumpIndex, dumpPath, outputDirectoryPath, cleanText, compress):
+def unpackDump(dumpPath, outputDirectoryPath, cleanText):
+    pages = []
+
+    try:
+        with gzip.open(dumpPath, 'rb') as dumpFile:
+            dumpText = dumpFile.read()
+
+        pageNames = [name.strip() for name in re.findall('^\[\[(?P<title>[^\]]+)\]\]\s?$', dumpText, flags=re.M)]
+
+        pageTexts = [pageText.strip() for pageText in re.split('^\[\[[^\]]+\]\]\s?$', dumpText, flags=re.M) if pageText]
+
+        pages = zip(pageNames, pageTexts)
+        pages = filter(filterPage, pages)
+
+        if cleanText:
+            cpuCount = multiprocessing.cpu_count()
+            pool = multiprocessing.Pool(processes=cpuCount)
+            pages = pool.map(cleanPage, pages)
+    except:
+        pass
+
+    return pages
+
+
+def prepareWikipediaDumps(inputDirectoryPath, outputDirectoryPath, cleanText=True, compress=True):
+    if os.path.exists(outputDirectoryPath):
+        shutil.rmtree(outputDirectoryPath, ignore_errors=True)
+        log.info('Output directory {0} has been removed.', outputDirectoryPath)
+
+    os.mkdir(outputDirectoryPath)
+    os.chown(outputDirectoryPath, 1000, 1000)
+    log.info('Output directory {0} has been created.', outputDirectoryPath)
+
+    pathName = inputDirectoryPath + '/*wiki*.txt.gz'
+    dumpPaths = glob.glob(pathName)
+    dumpsCount = len(dumpPaths)
+    log.info('Found {0} Wikipedia dumps.', dumpsCount)
+
+    startTime = time.time()
+    for dumpIndex, dumpPath in enumerate(dumpPaths):
+        pages = unpackDump(dumpPath, outputDirectoryPath, cleanText)
+
         dumpName = os.path.basename(dumpPath).split('.')[0]
-        pages = []
+
+        dumpDirectoryPath = os.path.join(outputDirectoryPath, dumpName)
+        os.mkdir(dumpDirectoryPath)
+        os.chown(dumpDirectoryPath, 1000, 1000)
+
+        for pageName, pageText in pages:
+            pageName = re.sub('[^a-zA-Z0-9\s\(\)]', '', pageName).strip()
+            savePage(dumpDirectoryPath, pageName, pageText, compress)
+
         currentTime = time.time()
-        elapsed = currentTime - self.startTime
-        secondsPerFile = elapsed
+        elapsed = currentTime - startTime
+        secondsPerFile = elapsed / (dumpIndex + 1)
 
-        try:
-            with gzip.open(dumpPath, 'rb') as dumpFile:
-                dumpText = dumpFile.read()
+        log.progress('Unpacking Wikipedia dumps: {0:.3f}%. Last dump: {1} ({2} pages). Elapsed: {3}. ({4:.3f} sec/file)',
+                     dumpIndex + 1,
+                     dumpsCount,
+                     dumpName,
+                     len(pages),
+                     timedelta(seconds=elapsed),
+                     secondsPerFile)
 
-            dumpDirectoryPath = os.path.join(outputDirectoryPath, dumpName)
-            os.mkdir(dumpDirectoryPath)
-            os.chown(dumpDirectoryPath, 1000, 1000)
-
-            pageNames = [name.strip() for name in re.findall('^\[\[(?P<title>[^\]]+)\]\]\s?$', dumpText, flags=re.M)]
-            pageTexts = [pageText.strip() for pageText in re.split('^\[\[[^\]]+\]\]\s?$', dumpText, flags=re.M) if pageText]
-
-            pages = zip(pageNames, pageTexts)
-            pages = filter(self.filterPage, pages)
-
-            if cleanText:
-                pages = map(self.cleanPage, pages)
-
-            for pageName, pageText in pages:
-                self.savePage(dumpDirectoryPath, pageName, pageText, compress)
-
-            log.progress('Unpacking Wikipedia dumps: {0:.3f}%. Last dump: {1} ({2} pages). Elapsed: {3}. ({4:.3f} sec/file)',
-                         dumpIndex + 1,
-                         self.dumpsCount,
-                         dumpName,
-                         len(pages),
-                         timedelta(seconds=elapsed),
-                         secondsPerFile)
-        except:
-            pass
-
-
-    def prepare(self, inputDirectoryPath, outputDirectoryPath, filterText=True, compress=True):
-        if os.path.exists(outputDirectoryPath):
-            shutil.rmtree(outputDirectoryPath, ignore_errors=True)
-            log.info('Output directory {0} has been removed.', outputDirectoryPath)
-
-        os.mkdir(outputDirectoryPath)
-        os.chown(outputDirectoryPath, 1000, 1000)
-        log.info('Output directory {0} has been created.', outputDirectoryPath)
-
-        pathName = inputDirectoryPath + '/*wiki*.txt.gz'
-        dumpPaths = glob.glob(pathName)
-        self.dumpsCount = len(dumpPaths)
-        log.info('Found {0} Wikipedia dumps.', self.dumpsCount)
-
-        processes = multiprocessing.cpu_count()
-        pool = multiprocessing.Pool(processes=processes)
-        for dumpIndex, dumpPath in enumerate(dumpPaths):
-            pool.apply_async(self.unpackDump, args=(dumpIndex, dumpPath, outputDirectoryPath, filterText, compress))
-        pool.close()
-        pool.join()
-
-        log.lineBreak()
-        log.info('Processing complete.')
+    log.lineBreak()
+    log.info('Processing complete.')
 
 if __name__ == '__main__':
     inputDirectoryPath = '../data/Wikipedia'
     outputDirectoryPath = '../data/Wikipedia_prepared'
 
-    wikipediaPreparator = WikipediaPreparator()
-    wikipediaPreparator.prepare(inputDirectoryPath, outputDirectoryPath)
+    prepareWikipediaDumps(inputDirectoryPath, outputDirectoryPath)
