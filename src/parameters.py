d38b3ce384291447f848db9870a43028ee3f5068
Yuriy Filonov <yuriy.filonov@gmail.com>
2015-10-02 11:53:33 +1000

    IndexContextProvider has been added.

--- src/parameters.py
+++ src/parameters.py
@@ -1,8 +1,65 @@
 import os
 import gzip
+import io
 import log
 import struct
 import collections
+
+
+class IndexContextProvider():
+    def __init__(self, contextsFilePath):
+        self.contextsFilePath = contextsFilePath
+        self.contextsFile = gzip.open(self.contextsFilePath, 'rb')
+        self.contextsCount, self.contextSize = self.readContextsShape()
+        self.bufferSize = self.contextSize * 4
+        self.contextFormat = '{0}i'.format(self.contextSize)
+
+
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            start = item.start or 0
+            stop = item.stop if item.stop <= self.contextsCount else self.contextsCount
+            step = item.step or 1
+            return [self[i] for i in xrange(start, stop, step)]
+
+        return self.getContext(item)
+
+
+    def __del__(self):
+        self.contextsFile.close()
+
+
+    def readContextsShape(self):
+        self.contextsFile.seek(0, io.SEEK_SET)
+
+        contextsCount = self.contextsFile.read(4)
+        contextSize = self.contextsFile.read(4)
+
+        self.contextsCount = struct.unpack('i', contextsCount)[0]
+        self.contextSize = struct.unpack('i', contextSize)[0]
+
+        return self.contextsCount, self.contextSize
+
+
+    def getContext(self, contextIndex):
+        contextPosition = contextIndex * self.bufferSize + 8 # 8 for contextsCount + contextSize
+
+        self.contextsFile.seek(contextPosition, io.SEEK_SET)
+
+        buffer = self.contextsFile.read(self.bufferSize)
+        context = struct.unpack(self.contextFormat, buffer)
+
+        return context
+
+
+class EmbeddingsProvider():
+    def __init__(self, embeddingsFilePath):
+        self.embeddingsFilePath = embeddingsFilePath
+
+    def getEmbedding(self):
+        with open(self.embeddingsFilePath, 'r') as embeddingsFile:
+            embedding = embeddingsFile.readall()
+            return embedding
 
 
 def dumpFileVocabulary(vocabulary, vocabularyFilePath):
@@ -30,6 +87,31 @@ def dumpFileVocabulary(vocabulary, vocabularyFilePath):
         file.flush()
 
         log.lineBreak()
+
+
+def loadFileVocabulary(vocabularyFilePath):
+    vocabulary = collections.OrderedDict()
+
+    with gzip.open(vocabularyFilePath, 'rb') as file:
+        itemsCount = file.read(4)
+        itemsCount = struct.unpack('i', itemsCount)[0]
+
+        for itemIndex in range(0, itemsCount):
+            wordLength = file.read(4)
+            wordLength = struct.unpack('i', wordLength)[0]
+
+            word = file.read(wordLength)
+
+            index = file.read(4)
+            index = struct.unpack('i', index)[0]
+
+            vocabulary[word] = index
+
+            log.progress('Loading file vocabulary: {0:.3f}%.', itemIndex + 1, itemsCount)
+
+        log.info('')
+
+    return vocabulary
 
 
 def dumpWordVocabulary(vocabulary, vocabularyFilePath):
@@ -62,31 +144,6 @@ def dumpWordVocabulary(vocabulary, vocabularyFilePath):
         log.lineBreak()
 
 
-def loadFileVocabulary(vocabularyFilePath):
-    vocabulary = collections.OrderedDict()
-
-    with gzip.open(vocabularyFilePath, 'rb') as file:
-        itemsCount = file.read(4)
-        itemsCount = struct.unpack('i', itemsCount)[0]
-
-        for itemIndex in range(0, itemsCount):
-            wordLength = file.read(4)
-            wordLength = struct.unpack('i', wordLength)[0]
-
-            word = file.read(wordLength)
-
-            index = file.read(4)
-            index = struct.unpack('i', index)[0]
-
-            vocabulary[word] = index
-
-            log.progress('Loading file vocabulary: {0:.3f}%.', itemIndex + 1, itemsCount)
-
-        log.info('')
-
-    return vocabulary
-
-
 def loadWordVocabulary(vocabularyFilePath):
     vocabulary = collections.OrderedDict()
 
@@ -113,6 +170,18 @@ def loadWordVocabulary(vocabularyFilePath):
         log.info('')
 
     return vocabulary
+
+
+def dumpEmbeddings(embeddings, embeddingsFilePath):
+    with open(embeddingsFilePath, 'w') as embeddingsFile:
+        embedding = embeddingsFile.readall()
+        return embedding
+
+
+def loadEmbeddings(embeddingsFilePath):
+    with open(embeddingsFilePath) as embeddingsFile:
+        embedding = embeddingsFile.readall()
+        return embedding
 
 
 def loadContexts(contextsFilePath):
--- src/trainmodel.py
+++ src/trainmodel.py
@@ -1,5 +1,33 @@
-def main():
-    pass
+import parameters
+import collections
+
+
+def trainModel():
+    fileVocabularyPath = '../data/Fake/Processed/file_vocabulary.bin.gz'
+    fileVocabulary = parameters.loadFileVocabulary(fileVocabularyPath)
+    fileIndexVocabulary = [(value, key) for key, value in fileVocabulary.items()]
+    fileIndexVocabulary = collections.OrderedDict(fileIndexVocabulary)
+
+    wordVocabularyPath = '../data/Fake/Processed/word_vocabulary.bin.gz'
+    wordVocabulary = parameters.loadWordVocabulary(wordVocabularyPath)
+    wordIndexVocabulary = [(value[0], key) for key, value in wordVocabulary.items()]
+    wordIndexVocabulary = collections.OrderedDict(wordIndexVocabulary)
+
+    contextsPath = '../data/Fake/Processed/contexts.bin.gz'
+    contextProvider = parameters.IndexContextProvider(contextsPath)
+
+    print 'Contexts count: {0}'.format(contextProvider.contextsCount)
+    print 'Context size: {0}'.format(contextProvider.contextSize)
+
+    for context in contextProvider[:]:
+        fileIndex = context[0]
+        wordIndices = context[1:]
+
+        file = fileIndexVocabulary[fileIndex]
+        words = map(lambda wordIndex: wordIndexVocabulary[wordIndex], wordIndices)
+
+        print '{0} {1}'.format(file, words)
+
 
 if __name__ == '__main__':
-    main()
+    trainModel()
